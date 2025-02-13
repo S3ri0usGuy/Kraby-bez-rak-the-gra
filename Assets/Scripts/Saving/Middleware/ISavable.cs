463500e12d227d4bec8930cd5504302117e0e4d7
romandykyi <roman.dykyi.8210@gmail.com>
2025-02-13 18:29:46 +0100

    Implement the middleware base

---
+++ Assets/Scripts/Saving/Middleware/ISavable.cs
@@ -0,0 +1,44 @@
+using System;
+
+/// <summary>
+/// An interface that represents an object that interacts
+/// with the save system (usually a Unity component).
+/// </summary>
+/// <remarks>
+/// <para>
+/// The most prominent class that implements this interface is the
+/// <see cref="SavableComponent{TData}" />.
+/// </para>
+/// <para>
+/// When implementing, make sure that the equals method is correct.
+/// For all C# classes without custom Equals override, and Unity objects this should not
+/// be a problem.
+/// </para>
+/// </remarks>
+public interface ISavable
+{
+	/// <summary>
+	/// Gets a unique identifier which is assigned to this object.
+	/// </summary>
+	public Guid id { get; }
+
+	/// <summary>
+	/// Loads a data into the object.
+	/// </summary>
+	/// <remarks>
+	/// Called when the object registers itself in the save system.
+	/// </remarks>
+	/// <param name="data">
+	/// An object to be loaded. Can be <see langword="null" /> if
+	/// the data was not found.
+	/// </param>
+	public void Load(object data);
+
+	/// <summary>
+	/// Retrieves a data to be saved.
+	/// </summary>
+	/// <returns>
+	/// An object which is needed to be saved.
+	/// </returns>
+	public object Save();
+}
---
+++ Assets/Scripts/Saving/Middleware/SaveSystem.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// A Unity object singleton which tracks savable objects,
+/// keeps the save slot in memory and allows data saving..
+/// </summary>
+public sealed class SaveSystem : SingletonMonoBehaviour<SaveSystem>
+{
+	private static SaveSlot _slot;
+
+	private readonly HashSet<ISavable> _savables = new();
+
+	/// <summary>
+	/// Starts tracking a savable object and initializes it with a save data.
+	/// </summary>
+	/// <remarks>
+	/// Don't forget to call the <see cref="RemoveSavable(ISavable)" />
+	/// when the savable is no longer needed.
+	/// </remarks>
+	/// <param name="savable">A savable object to initialize and start tracking.</param>
+	/// <exception cref="InvalidOperationException">
+	/// Thrown when an already added savable was attempted to be added
+	/// again.
+	/// </exception>
+	public void AddSavable(ISavable savable)
+	{
+		if (savable == null)
+			throw new ArgumentNullException(nameof(savable));
+
+		if (!_savables.Add(savable))
+			throw new InvalidOperationException("Attempted to add the same savable twice.");
+
+		var data = _slot.saveData.componenentsData.GetValueOrDefault(savable.id);
+		savable.Load(data);
+	}
+
+	/// <summary>
+	/// Stops tracking the savable object.
+	/// </summary>
+	/// <remarks>
+	/// Has no effect if the savable wasn't tracked.
+	/// </remarks>
+	/// <param name="savable">A savable object to stop tracking.</param>
+	public void RemoveSavable(ISavable savable)
+	{
+		if (savable == null)
+		{
+			throw new ArgumentNullException(nameof(savable));
+		}
+
+		_savables.Remove(savable);
+	}
+
+	/// <summary>
+	/// Sets the save slot used globally.
+	/// </summary>
+	/// <remarks>
+	/// <para>
+	/// This method works even if there is no active <see cref="SaveSystem" /> on the scene and
+	/// works between scenes.
+	/// </para>
+	/// <para>
+	/// <b>Note:</b> the passed save slot must be is not loaded in this method.
+	/// In order to load the save slot use the <see cref="SaveProviderFactory.CreateProvider" /> 
+	/// and <see cref="ISaveProvider.Load(SaveSlot)" /> methods.
+	/// </para>
+	/// </remarks>
+	/// <param name="slot">The save slot to be set as the current.</param>
+	public static void SetSave(SaveSlot slot)
+	{
+		_slot = slot ?? throw new ArgumentNullException(nameof(slot));
+	}
+
+	/// <summary>
+	/// Saves registered savable objects to the current slot.
+	/// </summary>
+	public void Save()
+	{
+		if (_slot == null)
+		{
+			throw new InvalidOperationException("Attempted to save but there is no current save slot set.");
+		}
+
+		foreach (var savable in _savables)
+		{
+			_slot.saveData.componenentsData[savable.id] = savable.Save();
+		}
+
+		ISaveProvider provider = SaveProviderFactory.CreateProvider();
+		provider.Save(_slot);
+	}
+}
+
+/// <summary>
+/// An action for the event which is called right before the player's data is saved.
+/// </summary>
+/// <param name="save">A save object which is the same as the <see cref="SaveSystem.save" />.</param>
+public delegate void SavingAction(SaveSlot save);
