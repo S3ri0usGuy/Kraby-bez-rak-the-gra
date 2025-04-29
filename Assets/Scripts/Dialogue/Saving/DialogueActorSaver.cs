4fda94b64fd7955b2c46f0ce92c16db3d0234bd3
romandykyi <roman.dykyi.8210@gmail.com>
2025-04-29 14:56:30 +0200

    Integrate shared group with the saves

--- Assets/Scripts/Dialogue/Saving/DialogueActorSaver.cs
+++ Assets/Scripts/Dialogue/Saving/DialogueActorSaver.cs
@@ -7,6 +7,8 @@ using UnityEngine;
 [RequireComponent(typeof(DialogueActor))]
 public class DialogueActorSaver : SavableComponent<DialogueSaveData>
 {
+	private DialogueGroup _sharedGroup;
+
 	private DialogueActor _actor;
 	private DialogueActor actor => _actor ? _actor : _actor = GetComponent<DialogueActor>();
 
@@ -16,11 +18,21 @@ public class DialogueActorSaver : SavableComponent<DialogueSaveData>
 
 	protected override DialogueSaveData fallbackData => new();
 
+	protected override void Awake()
+	{
+		_sharedGroup = SharedDialogueGroupProvider.exists ?
+			SharedDialogueGroupProvider.instance.sharedGroup : null;
+
+		base.Awake();
+	}
+
 	protected override void Validate(DialogueSaveData data)
 	{
 		if (string.IsNullOrEmpty(data.nextNodeName) || !_group) return;
 
-		if (!_group.nameToObject.ContainsKey(data.nextNodeName))
+		if (!_group.nameToObject.ContainsKey(data.nextNodeName) &&
+			(!_sharedGroup || !_sharedGroup.nameToObject.ContainsKey(data.nextNodeName))
+			)
 		{
 			Debug.LogWarning($"Couldn't find a dialogue node with \"{data.nextNodeName}\" name.", gameObject);
 			data.nextNodeName = null;
@@ -36,7 +48,9 @@ public class DialogueActorSaver : SavableComponent<DialogueSaveData>
 		}
 
 		if (!string.IsNullOrEmpty(data.nextNodeName) &&
-			_group.nameToObject.TryGetValue(data.nextNodeName, out var node))
+			(_group.nameToObject.TryGetValue(data.nextNodeName, out var node) ||
+			_sharedGroup && _sharedGroup.nameToObject.TryGetValue(data.nextNodeName, out node))
+			)
 		{
 			actor.SetNextNode(node);
 		}
