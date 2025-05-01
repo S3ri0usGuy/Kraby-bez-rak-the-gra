cd535db51fd85c3aad28049903972e177635496a
romandykyi <roman.dykyi.8210@gmail.com>
2025-05-02 00:12:38 +0200

    Integrate the delayed action with the saves

---
+++ Assets/Scripts/Control/Delayed/DelayedActionSaver.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+/// <summary>
+/// A component that saves the <see cref="DelayedAction" /> state.
+/// </summary>
+[DisallowMultipleComponent]
+[RequireComponent(typeof(DelayedActionSaver))]
+public class DelayedActionSaver : SavableComponent<DelayedActionSaveData>
+{
+	private DelayedAction _delayedAction;
+
+	protected override DelayedActionSaveData fallbackData => new() { timeLeft = -1f };
+
+	protected override void Awake()
+	{
+		_delayedAction = GetComponent<DelayedAction>();
+		base.Awake();
+	}
+
+	protected override void OnLoad()
+	{
+		if (data.timeLeft > 0f)
+		{
+			_delayedAction.Perform(data.timeLeft);
+		}
+	}
+
+	protected override void OnSave()
+	{
+		data.timeLeft = _delayedAction.timeLeft;
+	}
+}
