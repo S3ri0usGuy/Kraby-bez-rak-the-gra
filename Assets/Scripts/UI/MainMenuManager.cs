663cc1c56b7e228eb50e51e5daf3b4f881685f64
S3ri0usGuy <skx88craft@gmail.com>
2025-05-04 16:23:04 +0200

    Add Main Menu
    - Launches game
    - quitt the  game
    - loads at the sart
    - set up for settings, credits and save load system

---
+++ Assets/Scripts/UI/MainMenuManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//<Summary>
+//Controls Main Menu: Changes scene and quits game
+//</Summary>
+//TO DO: Integrate safe system, refactor change scene function
+
+public class MainMenuManager : MonoBehaviour //should be singleton ?
+{
+	[SerializeField] private string _continueGame;
+	public void ContineGame()
+	{
+		SceneManager.LoadSceneAsync(_continueGame);
+	}
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+}
