70970860efea3a34cfc7176dd644b5890c9948da
Roman Dykyi <roman.dykyi.8210@gmail.com>
2025-05-25 21:35:20 +0200

    Integrate main menu with saves (kinda)

--- Assets/Scripts/UI/MainMenuManager.cs
+++ Assets/Scripts/UI/MainMenuManager.cs
@@ -1,17 +1,43 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-//<Summary>
-//Controls Main Menu: Changes scene and quits game
-//</Summary>
-//TO DO: Integrate save system, refactor change scene function
-
-public class MainMenuManager : MonoBehaviour //should be singleton ?
+/// <Summary>
+/// Controls Main Menu: Changes scene and quits game
+/// </Summary>
+/// TO DO: Integrate save system better
+public class MainMenuManager : MonoBehaviour // should be singleton ? - nah, doesn't matter
 {
+	private SaveSlot _loadedSlot;
+
 	[SerializeField] private string _continueGame;
-	public void ContineGame()
+	[SerializeField] private Button _continueButton;
+	[SerializeField] private string _defaultSlotName = "default";
+
+	private void Start()
+	{
+		_loadedSlot = new(_defaultSlotName);
+		ISavePersistence persistence = SavePersistenceFactory.CreatePersistence();
+
+		bool isLoaded = persistence.Load(_loadedSlot);
+		_continueButton.gameObject.SetActive(isLoaded);
+	}
+
+	private void StartGame()
 	{
 		SceneManager.LoadSceneAsync(_continueGame);
+	}
+
+	public void NewGame()
+	{
+		SaveSlot newSlot = new(_defaultSlotName);
+		SaveSystem.SetSave(newSlot);
+		StartGame();
+	}
+	public void ContineGame()
+	{
+		SaveSystem.SetSave(_loadedSlot);
+		StartGame();
 	}
 	public void QuitGame()
 	{
