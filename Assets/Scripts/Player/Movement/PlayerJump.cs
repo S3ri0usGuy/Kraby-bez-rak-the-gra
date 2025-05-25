9a887466c61d55fc01902507018392bf0f942c2e
S3ri0usGuy <skx88craft@gmail.com>
2025-05-25 20:44:02 +0200

    Revert "zmiany"
    This reverts commit e4588a029f9da2c340f411c6b3be376689056a07.

--- Assets/Scripts/Player/Movement/PlayerJump.cs
+++ Assets/Scripts/Player/Movement/PlayerJump.cs
@@ -1,26 +1,50 @@
 using System;
 using UnityEngine;
+
 /// <summary>
-/// A component that provides player movement without jump mechanics.
+/// A component that provides the jump mechanic for the player.
 /// </summary>
 [RequireComponent(typeof(PlayerMovement))]
 public class PlayerJump : MonoBehaviour
 {
 	private PlayerMovement _movement;
+
+	[SerializeField]
+	[Tooltip("A vertical speed of the jump.")]
+	private ModifiableFloat _jumpVelocity = new(22f, minValue: 0f);
+
 	/// <summary>
-	/// An event that is triggered when the jump is attempted.
+	/// Gets a modifiable vertical speed of the jump.
+	/// </summary>
+	public ModifiableFloat jumpVelocity => _jumpVelocity;
+
+	/// <summary>
+	/// An event that is triggered when the jump is performed.
 	/// </summary>
 	public event Action<PlayerJump> performed;
+
 	private void Awake()
 	{
 		_movement = GetComponent<PlayerMovement>();
 	}
+
 	/// <summary>
-	/// This method is no longer needed as jumping is disabled.
+	/// Performs a jump if it's possible.
 	/// </summary>
+	/// <returns>
+	/// <see langword="true" /> if a jump was performed successfully;
+	/// otherwise <see langword="false" />.
+	/// </returns>
 	public bool Perform()
 	{
-		// Skok nie jest ju¿ mo¿liwy, wiêc zawsze zwracamy false.
-		return false;
+		if (!enabled || !gameObject.activeInHierarchy)
+			return false;
+		if (_movement.isFreezed || !_movement.isGrounded)
+			return false;
+
+		_movement.Jump(_jumpVelocity);
+		performed?.Invoke(this);
+
+		return true;
 	}
 }
